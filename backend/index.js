0dc64f054ca736c814dfcac89af4f310ed4041d3
Nikhil Bavistale <nikhilbavistale2022@vitbhopal.ac.in>
2024-02-08 21:25:56 +0530

    Connect to MongoDB Atlas

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB Connected")
+  })
+  .catch((err) => {
+    console.log(err)
+    });
+
 const app = express();
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
