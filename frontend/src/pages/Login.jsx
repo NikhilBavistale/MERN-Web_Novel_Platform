cc7910a41c9e0783e321f0383b9d281653a4ab2f
Nikhil Bavistale <nikhilbavistale2022@vitbhopal.ac.in>
2024-02-09 08:31:59 +0530

    Complete sign in page functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,116 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate=useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
 
-export default Login
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+      navigate("/")
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+  return (
+    <section className="flex flex-col md:flex-row h-screen items-center">
+      <div className="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
+        <img
+          src="https://source.unsplash.com/random"
+          alt=""
+          className="w-full h-full object-cover"
+        />
+      </div>
+
+      <div
+        className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
+      flex items-center justify-center"
+      >
+        <div className="w-full h-100">
+          <h1 className="text-xl md:text-2xl font-bold leading-tight mt-12">
+            Sign In to your account
+          </h1>
+
+          <form onSubmit={handleSubmit} className="mt-6">
+            <div>
+              <input
+                type="email"
+                id="email"
+                onChange={handleChange}
+                placeholder="Enter Email Address"
+                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
+                autoFocus
+                required
+              />
+            </div>
+
+            <div className="mt-4">
+              <input
+                type="password"
+                id="password"
+                onChange={handleChange}
+                placeholder="Enter Password"
+                minLength="6"
+                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
+              focus:bg-white focus:outline-none"
+                required
+              />
+            </div>
+
+            <button
+              disabled={loading}
+              type="submit"
+              className="w-full block bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-400 text-white font-semibold rounded-lg
+            px-4 py-3 mt-6"
+            >
+              {loading ? "Loading..." : "Sign In"}
+            </button>
+          </form>
+
+          {error ? (
+            <p className="text-red-600 text-base">
+              {" "}
+              "Email or Password is not Correct:"{" "}
+            </p>
+          ) : (
+            ""
+          )}
+          <p className="mt-8">
+            Don't have an account yet?{" "}
+            <Link
+              to="/register"
+              className="text-blue-500 hover:text-blue-700 font-semibold"
+            >
+              Sign Up
+            </Link>{" "}
+            Here
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
