5b39f9d00648bee20cb53d9572f9753ef7fa9552
Nikhil Bavistale <nikhilbavistale2022@vitbhopal.ac.in>
2024-02-09 08:05:58 +0530

    Create sign in api route

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,5 +1,7 @@
 import User from "../models/UserModel.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   //   console.log(req.body);
@@ -11,6 +13,33 @@ export const signup = async (req, res, next) => {
     res.status(201).json({
       message: "Signup success! Please login.",
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User Not Found"));
+    }
+    const isPasswordValid = await bcryptjs.compare(
+      password,
+      validUser.password
+    );
+    if (!isPasswordValid) {
+      return next(errorHandler(401, "Wrong Credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    const { password: hashedPassword, ...rest} = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
