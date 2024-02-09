4497b386b36e1f07114eba784cd7e7dd79d60beb
Nikhil Bavistale <nikhilbavistale2022@vitbhopal.ac.in>
2024-02-09 20:27:56 +0530

    Upload image function

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,24 +1,85 @@
-import { useSelector } from "react-redux";
 import { Button, Label, Textarea } from "flowbite-react";
-import { useState } from "react";
-
+import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 const Profile = () => {
+  const fileRef = useRef(null);
+  const [image, setImage] = useState(undefined);
+  const [imagePercent, setImagePercent] = useState(0);
+  const [imageError, setImageError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+  useEffect(() => {
+    if (image) {
+      handleFileUpload(image);
+    }
+  }, [image]);
+ 
+  const handleFileUpload = async (image) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImagePercent(Math.round(progress));
+      },
+      (error) => {
+        setImageError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, profilePicture: downloadURL })
+        );
+      }
+    );
+  };
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
-  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="my-7 text-3xl font-bold text-center">Profile</h1>
       <form className="flex flex-col gap-4">
         {/* Profile Picture  */}
-        <img
-          src={currentUser.profilePicture}
-          alt="Profile"
-          className="h-30 w-30 self-center cursor-pointer rounded-full object-cover mt-2"
+        <input
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+          onChange={(e) => setImage(e.target.files[0])}
         />
-
+        <img
+          src={formData.profilePicture || currentUser.profilePicture}
+          alt="profile"
+          className="h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2"
+          onClick={() => fileRef.current.click()}
+        />
+        <p className="text-sm self-center">
+          {imageError ? (
+            <span className="text-red-700">
+              Error uploading image (file size must be less than 2 MB)
+            </span>
+          ) : imagePercent > 0 && imagePercent < 100 ? (
+            <span className="text-slate-700">{`Uploading: ${imagePercent} %`}</span>
+          ) : imagePercent === 100 ? (
+            <span className="text-green-700">Image uploaded successfully</span>
+          ) : (
+            ""
+          )}
+        </p>
         {/* first row contains username and email */}
         <div className="flex gap-8">
           {/* Username  */}
@@ -112,16 +173,10 @@ const Profile = () => {
         <Button type="submit" className="mt-10">
           Save
         </Button>
-        <div className='flex justify-between mt-5'>
-        <span
-          className='text-red-700 cursor-pointer'
-        >
-          Delete Account
-        </span>
-        <span className='text-red-700 cursor-pointer'>
-          Sign out
-        </span>
-      </div>
+        <div className="flex justify-between mt-5">
+          <span className="text-red-700 cursor-pointer">Delete Account</span>
+          <span className="text-red-700 cursor-pointer">Sign out</span>
+        </div>
       </form>
     </div>
   );
