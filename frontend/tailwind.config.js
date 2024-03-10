a84e84cbf4c6dae7e541205d6d81746f6aa9ff5d
Nikhil Bavistale <nikhilbavistale2022@vitbhopal.ac.in>
2024-03-10 21:24:11 +0530

    Added dependencies

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,20 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      white: "#ffffff",
+      cornflower: "#8ecae6",
+      "pacific-blue": "#219ebc",
+      elephant: "#023047",
+      "selective-yellow": "#ffb703",
+      "gold-drop": "#fb8500",
+      // homepage
+      nyanza: "#D0E3CC",
+      "light-yellow": "#F7FFDD",
+      mindaro: "#FCFDAF",
+      flax: "#EFD780",
+      "earth-yellow": "#DBA159",
+    },
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [require("flowbite/plugin"), require("tailwind-scrollbar")],
 };
