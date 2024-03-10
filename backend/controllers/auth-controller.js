1c6f13eb2811d3053bee3b250d8887fe14ba73f0
Nikhil Bavistale <nikhilbavistale2022@vitbhopal.ac.in>
2024-03-10 20:45:13 +0530

    Added more auth options

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,18 +1,33 @@
-import User from "../models/UserModel.js";
-import bcryptjs from "bcryptjs";
-import { errorHandler } from "../utils/error.js";
-import jwt from "jsonwebtoken";
+import User from '../models/UserModel.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
-  //   console.log(req.body);
   const { username, email, password } = req.body;
-  const hashedPassword = await bcryptjs.hashSync(password, 12);
-  const newUser = new User({ username, email, password: hashedPassword });
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === '' ||
+    email === '' ||
+    password === ''
+  ) {
+    next(errorHandler(400, 'All fields are required'));
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
   try {
     await newUser.save();
-    res.status(201).json({
-      message: "Signup success! Please login.",
-    });
+    res.json('Signup successful');
   } catch (error) {
     next(error);
   }
@@ -20,17 +35,32 @@ export const signup = async (req, res, next) => {
 
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
+
+  if (!email || !password || email === '' || password === '') {
+    next(errorHandler(400, 'All fields are required'));
+  }
+
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler(404, "User not found"));
+    if (!validUser) {
+      return next(errorHandler(404, 'User not found'));
+    }
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, "wrong credentials"));
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-    const { password: hashedPassword, ...rest } = validUser._doc;
-    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+    if (!validPassword) {
+      return next(errorHandler(400, 'Invalid password'));
+    }
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
+
+    const { password: pass, ...rest } = validUser._doc;
+
     res
-      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
       .status(200)
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
       .json(rest);
   } catch (error) {
     next(error);
@@ -38,43 +68,48 @@ export const signin = async (req, res, next) => {
 };
 
 export const google = async (req, res, next) => {
+  const { email, name, googlePhotoUrl } = req.body;
   try {
-    const user = await User.findOne({ email: req.body.email });
+    const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-      const { password: hashedPassword, ...rest } = user._doc;
-      const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET
+      );
+      const { password, ...rest } = user._doc;
       res
-        .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
         .status(200)
+        .cookie('access_token', token, {
+          httpOnly: true,
+        })
         .json(rest);
     } else {
-      const generatedPassword = Math.random().toString(36).slice(-8)+Math.random().toString(36).slice(-8);
-      const hashedPassword = await bcryptjs.hashSync(generatedPassword, 12);
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
       const newUser = new User({
         username:
-          req.body.name.split(" ").join("").toLowerCase() +
-          Math.floor(Math.random() * 10000).toString(),
-        email: req.body.email,
+          name.toLowerCase().split(' ').join('') +
+          Math.random().toString(9).slice(-4),
+        email,
         password: hashedPassword,
-        profilePicture: req.body.photo,
+        profilePicture: googlePhotoUrl,
       });
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
-      const { password: hashedPassword2, ...rest } = newUser._doc;
-      const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+      const token = jwt.sign(
+        { id: newUser._id, isAdmin: newUser.isAdmin },
+        process.env.JWT_SECRET
+      );
+      const { password, ...rest } = newUser._doc;
       res
-        .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
         .status(200)
+        .cookie('access_token', token, {
+          httpOnly: true,
+        })
         .json(rest);
-    
     }
   } catch (error) {
     next(error);
   }
-};
-
-export const signout = (req, res) => {
-  res.clearCookie("access_token");
-  res.status(200).json({ message: "Signout success" });
 };
